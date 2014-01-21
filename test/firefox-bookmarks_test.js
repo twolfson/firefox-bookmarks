98c7e19ba278ae90e76e48c828f3c3602784b7a0
Todd Wolfson <todd@twolfson.com>
2014-01-21 02:44:40 -0800

    Fixed up remaining variable definitions

--- test/firefox-bookmarks_test.js
+++ test/firefox-bookmarks_test.js
@@ -18,8 +18,8 @@ describe('bookmarks', function () {
   describe('formatting a simple set of bookmarks', function () {
     before(function () {
       // Prepare globals for bookmarks
-      var input = require('./test_files/simple.input.json'),
-          options = require('./test_files/simple.config.json');
+      var input = require('./test_files/simple.input.json');
+      var options = require('./test_files/simple.config.json');
 
       // Parse the bookmarks
       this.output = parser(input, options);
@@ -28,8 +28,8 @@ describe('bookmarks', function () {
 
     it('returns an matching array of bookmarks', function () {
       // Compare actual output to expected output
-      var actualBookmarks = this.output,
-          expectedBookmarks = require('./expected_files/simple.output.json');
+      var actualBookmarks = this.output;
+      var expectedBookmarks = require('./expected_files/simple.output.json');
       assert.deepEqual(actualBookmarks, expectedBookmarks);
     });
   });
@@ -37,8 +37,8 @@ describe('bookmarks', function () {
   describe('formatting a nested set of bookmarks', function () {
     before(function () {
       // Prepare globals for bookmarks
-      var input = require('./test_files/nested.input.json'),
-          options = require('./test_files/nested.config.json');
+      var input = require('./test_files/nested.input.json');
+      var options = require('./test_files/nested.config.json');
 
       // Parse the bookmarks
       this.output = parser(input, options);
@@ -47,8 +47,8 @@ describe('bookmarks', function () {
 
     it('returns an matching array of bookmarks', function () {
       // Compare actual output to expected output
-      var actualBookmarks = this.output,
-          expectedBookmarks = require('./expected_files/nested.output.json');
+      var actualBookmarks = this.output;
+      var expectedBookmarks = require('./expected_files/nested.output.json');
       assert.deepEqual(actualBookmarks, expectedBookmarks);
     });
   });
