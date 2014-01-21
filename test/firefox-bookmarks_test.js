5d2674dfa0cdaf9948e20a5b9b8f3a3a2661b2e7
Todd Wolfson <todd@twolfson.com>
2014-01-21 02:23:55 -0800

    Copy/paste from twolfson/bookmarks

--- test/firefox-bookmarks_test.js
+++ test/firefox-bookmarks_test.js
@@ -1,11 +1,44 @@
-var firefox_bookmarks = require('../');
+// Load in assert
+var assert = require('assert'),
+    parser = require('../');
 
-describe('firefox-bookmarks', function () {
-  before(function () {
+// Basic test
+describe('bookmarks', function () {
+  describe('formatting a simple set of bookmarks', function () {
+    before(function () {
+      // Prepare globals for bookmarks
+      var input = require('./test_files/simple.input.json'),
+          options = require('./test_files/simple.config.json');
 
+      // Parse the bookmarks
+      this.output = parser(input, options);
+      // require('grunt').file.write(__dirname + '/actual_files/simple.output.json', JSON.stringify(this.output, null, 4));
+    });
+
+    it('returns an matching array of bookmarks', function () {
+      // Compare actual output to expected output
+      var actualBookmarks = this.output,
+          expectedBookmarks = require('./expected_files/simple.output.json');
+      assert.deepEqual(actualBookmarks, expectedBookmarks);
+    });
   });
 
-  it('', function () {
+  describe('formatting a nested set of bookmarks', function () {
+    before(function () {
+      // Prepare globals for bookmarks
+      var input = require('./test_files/nested.input.json'),
+          options = require('./test_files/nested.config.json');
 
+      // Parse the bookmarks
+      this.output = parser(input, options);
+      require('grunt').file.write(__dirname + '/actual_files/nested.output.json', JSON.stringify(this.output, null, 4));
+    });
+
+    it('returns an matching array of bookmarks', function () {
+      // Compare actual output to expected output
+      var actualBookmarks = this.output,
+          expectedBookmarks = require('./expected_files/nested.output.json');
+      assert.deepEqual(actualBookmarks, expectedBookmarks);
+    });
   });
 });
